98310ea747b249b015f97a15456c46abe61a1af2
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2020-12-01 08:13:25 +0100

    Some cleaning.

--- aoc2020-01.py
+++ aoc2020-01.py
@@ -6,15 +6,16 @@ with open('aoc2020-01-input.txt', 'r') as f:
 def partone():
     print('Advent of Code 2020, day 1 part 1')
     for (i,a) in enumerate(lines):
-        for (j,b) in enumerate(lines[i:]):
+        for b in lines[i:]:
             if int(a)+int(b) == 2020:
                 print(int(a)*int(b))
+                return
 
 def parttwo():
     print('Advent of Code 2020, day 1 part 2')
     for (i,a) in enumerate(lines):
         for (j,b) in enumerate(lines[i:]):
-            for (k,c) in enumerate(lines[j:]):
+            for c in lines[j:]:
                 if int(a) + int(b) + int(c) == 2020:
                     print(int(a)*int(b)*int(c))
                     return
