9472e5e658a34e7d02faef8826653423097c797d
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2020-12-01 08:05:35 +0100

    First day, yay!

---
+++ aoc2020-01.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+with open('aoc2020-01-input.txt', 'r') as f:
+  lines = f.read().split('\n')[:-1]
+
+def partone():
+    print('Advent of Code 2020, day 1 part 1')
+    for (i,a) in enumerate(lines):
+        for (j,b) in enumerate(lines[i:]):
+            if int(a)+int(b) == 2020:
+                print(int(a)*int(b))
+
+def parttwo():
+    print('Advent of Code 2020, day 1 part 2')
+    for (i,a) in enumerate(lines):
+        for (j,b) in enumerate(lines[i:]):
+            for (k,c) in enumerate(lines[j:]):
+                if int(a) + int(b) + int(c) == 2020:
+                    print(int(a)*int(b)*int(c))
+                    return
+partone()
+parttwo()
