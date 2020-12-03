1603080beb781ff914ca0e3b9d6db6d945b8b216
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2020-12-03 21:36:17 +0100

    Cleaning up day 3 code a bit.

--- aoc2020-03.py
+++ aoc2020-03.py
@@ -3,30 +3,31 @@
 with open('aoc2020-03-input.txt', 'r') as f:
   lines = f.read().split('\n')[:-1]
 
+# Test input.
 #lines = ['..##.......','#...#...#..','.#....#..#.','..#.#...#.#','.#...##..#.','..#.##.....','.#.#.#....#','.#........#','#.##...#...','#...##....#','.#..#...#.#']
 
 def partone(x, y):
     trees = 0
-    for (i, l) in enumerate(lines):
-        pos = (x*i)
-        linenumber = (i*y)
-        if linenumber > len(lines):
-            return(trees)
-        else:
-            line = lines[linenumber]
-            if pos >= len(line):
-                newpos = pos % len(line)
-                pos = newpos
-            if line[pos] == '#':
-                trees +=1
+    i = 0
+    while i < len(lines) and (i*y) <= len(lines):
+        xpos = (i*x)
+        ypos = (i*y)
+        #if ypos > len(lines):
+        #    return(trees)
+        #else:
+        line = lines[ypos]
+        if xpos >= len(line):
+            xpos = xpos % len(line)
+        if line[xpos] == '#':
+            trees +=1
+        i += 1
     return(trees)
 
 def parttwo():
     slopes = [(1,1), (3,1), (5,1), (7,1), (1,2)]
     number = 1
     for (x,y) in slopes:
-        new = partone(x,y)
-        number = number * new
+        number = number * partone(x,y)
     return(number)
 
 print('Advent of Code 2020, day 3 part 1')
