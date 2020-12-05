28ded9d26b5f6c6f6c91bb2f0a0e02628ec2c601
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2020-12-05 11:01:01 +0100

    More cleaning.

--- aoc2020-05.py
+++ aoc2020-05.py
@@ -11,12 +11,11 @@ for s in seats:
     IDs = IDs + [int(s.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), base=2)]
 
 def missing(IDs):
-    IDs.sort()
     for (i , id) in enumerate(IDs[:-1]):
         if (id + 2) == IDs[i+1]:
-            return(id+1)
+            return(id + 1)
 
-print('Advent of Code 2020, day 4 part 1')
+print('Advent of Code 2020, day 5 part 1')
 print(max(IDs))
-print('Advent of Code 2020, day 4 part 2')
-print(missing(IDs))
+print('Advent of Code 2020, day 5 part 2')
+print(missing(sorted(IDs)))
