382b098c50d2f5bb5445f5a83495386a6252e4a2
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2020-12-05 12:39:18 +0100

    Writing error corrected.

--- aoc2020-01.py
+++ aoc2020-01.py
@@ -17,7 +17,7 @@ def parttwo():
                     return(int(a)*int(b)*int(c))
 
 def partone2(): #Take 2, with list comprehension.
-    return[int(a)*int(b) for (i,a) in enumerate(lines) for b in lines[i:] if int(a)+int(b) == 2020]
+    return([int(a)*int(b) for (i,a) in enumerate(lines) for b in lines[i:] if int(a)+int(b) == 2020][0])
 
 def parttwo2(): #List comprehension, takes ages to run ...
     return([int(a)*int(b)*int(c) for (i,a) in enumerate(lines) for (j,b) in enumerate(lines[i:]) for c in lines[j:] if sum([int(a), int(b), int(c)]) == 2020][0])
