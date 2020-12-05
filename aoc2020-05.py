df12f56f394683bd2d3f32cbfb1f50afd1b14311
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2020-12-05 23:55:52 +0100

    Tried an even shorter day 5.

--- aoc2020-05.py
+++ aoc2020-05.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
 
 with open('aoc2020-05-input.txt', 'r') as f:
-    seats = f.read().split('\n')[:-1]
-
-#Test data
-#seats = ['BFFFBBFRRR','FFFBBBFRRR', 'BBFFBBFRLL']
-
-IDs = []
-for s in seats:
-    IDs.append(int(s.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), base=2))
+    seats = [int(line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2) for line in f]
 
 def missing(IDs):
     return(next(id + 1 for (i, id) in enumerate(IDs[:-1]) if id + 2 == IDs[i+1])) # Trying compact style
 
 print('Advent of Code 2020, day 5 part 1')
-print(max(IDs))
+print(max(seats))
 print('Advent of Code 2020, day 5 part 2')
-print(missing(sorted(IDs)))
+print(missing(sorted(seats)))
