77fe02ecb49f197d4138832cb8d411f184f5d01d
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2020-12-05 14:42:31 +0100

    New take on day 5.

--- aoc2020-05.py
+++ aoc2020-05.py
@@ -11,9 +11,7 @@ for s in seats:
     IDs.append(int(s.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), base=2))
 
 def missing(IDs):
-    for (i , id) in enumerate(IDs[:-1]):
-        if (id + 2) == IDs[i+1]:
-            return(id + 1)
+    return(next(id + 1 for (i, id) in enumerate(IDs[:-1]) if id + 2 == IDs[i+1])) # Trying compact style
 
 print('Advent of Code 2020, day 5 part 1')
 print(max(IDs))
