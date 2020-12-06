607474a5171b9331d0093ea7517bd98da5b245d0
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2020-12-06 09:52:33 +0100

    Day 6.

---
+++ aoc2020-06.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import re
+
+with open('aoc2020-06-input.txt', 'r') as f:
+    lines = f.read().strip().split('\n\n')
+
+answers = []
+for l in lines:
+    answers.append(l.split('\n'))
+
+#answers = [['abc'], ['a', 'b', 'c'], ['ab', 'ac'], ['a', 'a', 'a', 'a'], ['b']]
+
+def partone(ans):
+    count = 0
+    for group in ans:
+        grset = set(group[0])
+        for p in group:
+            grset.update(p)
+        count += len(grset)
+    return(count)
+
+def parttwo(ans):
+    count = 0
+    for group in ans:
+        grset = set(group[0])
+        for p in group:
+            grset = grset & set(p)
+        count += len(grset)
+    return(count)
+
+print('Advent of Code 2020, day 4 part 1')
+print(partone(answers))
+print('Advent of Code 2020, day 4 part 2')
+print(parttwo(answers))
