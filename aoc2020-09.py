d9da2dcbed771dada580111d924f09635d1c539a
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2020-12-09 08:26:12 +0100

    Day 9, part 2.

--- aoc2020-09.py
+++ aoc2020-09.py
@@ -9,19 +9,26 @@ with open('aoc2020-09-input.txt', 'r') as f:
 
 def partone():
     pre = 25
-    for (i, n) in enumerate(numbers[:]):
+    for (i, n) in enumerate(numbers):
         if i < pre:
             pass
         else:
-            item = n
             check = numbers[i-pre:i]
             try:
-                if next(True for (i, one) in enumerate(check) for two in check[i:] if one+two == item):
+                if next(True for (i, one) in enumerate(check) for two in check[i:] if one+two == n):
                     pass
             except StopIteration:
                 return(n)
 
+def parttwo():
+    n = partone()
+    for i in range(len(numbers)):
+        for j in range(i+2, len(numbers)):
+            if sum(numbers[i:j]) == n:
+                print(numbers[i:j])
+                return(max(numbers[i:j]) + min(numbers[i:j]))
+
 print('Advent of Code 2020, day 8 part 1')
 print(partone())
 print('Advent of Code 2020, day 8 part 2')
-#print(parttwo())
+print(parttwo())
