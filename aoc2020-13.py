a67cbfd057eabd2c3f315da483405de9633451a1
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2020-12-19 09:36:01 +0100

    Day 13, part 2.

--- aoc2020-13.py
+++ aoc2020-13.py
@@ -2,7 +2,12 @@
 with open('aoc2020-13-input.txt', 'r') as f:
     lines = f.read().strip().split('\n')
 
-#lines = [939, '7,13,x,x,59,x,31,19']
+#lines = [939, '7,13,x,x,59,x,31,19'] #1068781
+#lines = [0, '17,x,13,19'] #3417
+#lines = [0, '67,7,59,61'] #754018
+#lines = [0, '67,x,7,59,61'] #779210
+#lines = [0, '67,7,x,59,61'] #1261476
+#lines = [0, '1789,37,47,1889'] #1202161486
 
 time = int(lines[0])
 buses = lines[1].split(',')
@@ -19,7 +24,33 @@ def partone():
     (t, l) = min(waiting)
     return(t*l)
 
+def checkbuses(t, timetable):
+    for (time, ID) in timetable:
+        if (t + time) % ID != 0:
+            return(False)
+    return(True)
+
+def parttwo():
+    hitlist = []
+    for (i, b) in enumerate(buses):
+        if b != 'x':
+            hitlist.append((i,int(b)))
+    (t1, ID1) = hitlist[0]
+    i = 1
+    t = ID1 - t1
+    dt = 1
+
+    while i <= len(hitlist):
+        if checkbuses(t, hitlist):
+            return(t)
+        if checkbuses(t, hitlist[:i]):
+            (_, bt) = hitlist[i-1]
+            dt *= bt
+            i += 1
+        t += dt
+    return(0) #Something went wrong ...
+
 print('Advent of Code 2020, day 13 part 1')
 print(partone())
-#print('Advent of Code 2020, day 13 part 2')
-#print(parttwo())
+print('Advent of Code 2020, day 13 part 2')
+print(parttwo())
