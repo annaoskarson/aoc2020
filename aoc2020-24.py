03f2406dac5d96c25a8b25fdaff98ca6957f1d10
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2020-12-26 00:05:35 +0100

    Update aoc2020-24.py

--- aoc2020-24.py
+++ aoc2020-24.py
@@ -36,14 +36,13 @@ def move(start, instr):
     return(x,y)
 
 def partone():
-    black = []
+    black = set()
     for i in instructions:
         tile = move((0,0), i)
         if tile in black:
-            black.pop(black.index(tile))
+            black.remove(tile)
         else:
-            black.append(tile)
-        #print(tile,'---', black)
+            black.add(tile)
     return(black)
 
 def adjacent(x,y):
@@ -57,22 +56,25 @@ def dayflip(b, size):
         for y in range(ymin, ymax+1):
             adj = adjacent(x,y)
             baround = len([tile for tile in adj if tile in b])
-            if (x,y) in b and (baround == 0 or baround > 2):
-                towhite.append((x,y))
-            elif (x,y) not in b and baround == 2:
+            if (x,y) not in b and baround == 2:
                 toblack.append((x,y))
+    for (x,y) in b:
+        adj = adjacent(x,y)
+        baround = len([(x,y) for tile in adj if tile in b])
+        if baround == 0 or baround > 2:
+            towhite.append((x,y))
     for tile in towhite:
         b.remove(tile)
     for tile in toblack:
         b.add(tile)
     return(b)
 
+def size(flipped):
+    return([(min(flipped, key=lambda x:x[0])[0]-2, min(flipped, key=lambda x:x[1])[1]-1), \
+    (max(flipped, key=lambda x:x[0])[0]+2, max(flipped, key=lambda x:x[1])[1]+1)])
 
 def parttwo():
     black = set(partone())
-    def size(flipped):
-        s = [(min(flipped, key=lambda x:x[0])[0]-2, min(flipped, key=lambda x:x[1])[1]-1), (max(flipped, key=lambda x:x[0])[0]+2, max(flipped, key=lambda x:x[1])[1]+1)]
-        return(s)
     for i in range(1,101):
         floorsize = size(black)
         black = dayflip(black, floorsize)
