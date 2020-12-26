fd8478a4840ebaebfed1792742ebf90d6e47ac94
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2020-12-26 20:31:22 +0100

    Day 18, part 2.

--- aoc2020-18.py
+++ aoc2020-18.py
@@ -51,7 +51,53 @@ def partone():
         result.append((solve(hw)))
     return(sum(result))
 
+def calculate2(expr):
+    i = 1
+
+    while i < len(expr):
+        if expr[i] == '+':
+            expr = expr[:i-1] + [expr[i-1] + expr[i+1]] + expr[i+2:]
+        elif expr[i] == '*':
+            i += 2
+
+    i = 1
+    result = expr[0]
+    while i < len(expr):
+        if expr[i] == '*':
+            result = result * expr[i+1]
+            i += 2
+
+    return(result)
+
+def solve2(row):
+    up = []
+    i = 0
+    row = '(' + row + ')'
+    expr = []
+    while i < len(row):
+        if re.match(r'^[0-9]', row[i:]):
+            expr.append(int(row[i]))
+        elif re.match(r'^\*', row[i:]):
+            expr.append(row[i])
+        elif re.match(r'^\+', row[i:]):
+            expr.append(row[i])
+        elif re.match(r'^\(', row[i:]):
+            up.append(len(expr))
+        elif re.match(r'^\)', row[i:]):
+            start = up.pop()
+            result = calculate2(expr[start:])
+            expr = expr[:start] + [result]
+        i += 1
+    return(calculate2(expr))
+
+def parttwo():
+    result = []
+    for hw in homework:
+        hw = hw.replace(' ', '')
+        result.append((solve2(hw)))
+    return(sum(result))
+
 print('Advent of Code 2020, day 18 part 1')
 print(partone()) # too low
-#print('Advent of Code 2020, day 18 part 2')
-#print(parttwo())
+print('Advent of Code 2020, day 18 part 2')
+print(parttwo())
